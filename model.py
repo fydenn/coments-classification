8fb7bdb901604d67b7c41f676ecb15e2ce4beddc
fydenn <glebpotv@gmail.com>
2025-04-07 15:19:58 +0300

    last changes

--- model.py
+++ model.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import torch
 import torch.nn as nn
 
@@ -26,33 +25,4 @@ class CharLM(nn.Module):
 
         
 
-=======
-import torch
-import torch.nn as nn
-
-class CharLM(nn.Module):
-    def __init__(
-        self, hidden_dim: int, vocab_size: int, num_classes: int = 2
-        ):
-        super().__init__()
-        self.embedding = nn.Embedding(vocab_size, hidden_dim)
-        self.rnn = nn.RNN(hidden_dim, hidden_dim, batch_first=True)
-        self.linear = nn.Linear(hidden_dim, hidden_dim)
-        self.projection = nn.Linear(hidden_dim, num_classes)
-
-        self.non_lin = nn.Tanh()
-        self.dropout = nn.Dropout(p=0.1)
-        
-    def forward(self, input_batch) -> torch.Tensor:
-        embeddings = self.embedding(input_batch)  # [batch_size, seq_len, hidden_dim]
-        output, _ = self.rnn(embeddings)  # [batch_size, seq_len, hidden_dim]
-        output = output.mean(dim=1) #[batch_size, hidden_dim]
-        
-
-        output = self.dropout(self.linear(self.non_lin(output)))  # [batch_size, hidden_dim]
-        prediction = self.projection(self.non_lin(output)) # [batch_size, num_classes]
-
-        
-
->>>>>>> db0e71a901c0c3caf67e452e780b1c6d4a546b1b
         return prediction
